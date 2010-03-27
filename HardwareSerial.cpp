e6df4234e54b0bf2b0e8c4d2ef1777d1ac963057
dgummel <dgummel@893d1da4-2928-11de-946d-d3a74f8bd5dd>
2010-03-27 00:13:47 +0000

    changed HarwareSerial to optimized one

--- HardwareSerial.cpp
+++ HardwareSerial.cpp
@@ -1,6 +1,6 @@
 /*
-  HardwareSerial.cpp - Hardware serial library for Wiring
-  Copyright (c) 2006 Nicholas Zambetti.  All right reserved.
+  HardwareSerial.cpp - Improved HardwareSerial library for Arduino
+  Copyright (c) 2009 Kiril Zyapkov.  All right reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -16,205 +16,175 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
   
-  Modified 23 November 2006 by David A. Mellis
 */
-
-#include <stdio.h>
-#include <string.h>
-#include <inttypes.h>
-#include <stdlib.h>
-#include "wiring.h"
-#include "wiring_private.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
 #include "HardwareSerial.h"
 
-// Define constants and variables for buffering incoming serial data.  We're
-// using a ring buffer (I think), in which rx_buffer_head is the index of the
-// location to which to write the next incoming character and rx_buffer_tail
-// is the index of the location from which to read.
-#define RX_BUFFER_SIZE 128
+/**
+ * 2010-03-26
+ * using optimized HardwareSerial lib from
+ * http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1242466935/0
+ */
 
-struct ring_buffer {
-  unsigned char buffer[RX_BUFFER_SIZE];
-  int head;
-  int tail;
-};
+#define USART_RX_BUFFER_MASK (USART_RX_BUFFER_SIZE-1)
+#define USART_TX_BUFFER_MASK (USART_TX_BUFFER_SIZE-1)
 
-ring_buffer rx_buffer = { { 0 }, 0, 0 };
-
-#if defined(__AVR_ATmega1280__)
-ring_buffer rx_buffer1 = { { 0 }, 0, 0 };
-ring_buffer rx_buffer2 = { { 0 }, 0, 0 };
-ring_buffer rx_buffer3 = { { 0 }, 0, 0 };
+#if ( USART_RX_BUFFER_SIZE & USART_RX_BUFFER_MASK )
+#    error RX buffer size is not a power of 2
+#endif
+#if ( USART_TX_BUFFER_SIZE & USART_TX_BUFFER_MASK )
+#    error TX buffer size is not a power of 2
 #endif
 
-inline void store_char(unsigned char c, ring_buffer *rx_buffer)
-{
-  int i = (rx_buffer->head + 1) % RX_BUFFER_SIZE;
-
-  // if we should be storing the received character into the location
-  // just before the tail (meaning that the head would advance to the
-  // current location of the tail), we're about to overflow the buffer
-  // and so we don't write the character or advance the head.
-  if (i != rx_buffer->tail) {
-    rx_buffer->buffer[rx_buffer->head] = c;
-    rx_buffer->head = i;
-  }
-}
-
-#if defined(__AVR_ATmega1280__)
-
-SIGNAL(SIG_USART0_RECV)
-{
-  unsigned char c = UDR0;
-  store_char(c, &rx_buffer);
-}
-
-SIGNAL(SIG_USART1_RECV)
-{
-  unsigned char c = UDR1;
-  store_char(c, &rx_buffer1);
-}
-
-SIGNAL(SIG_USART2_RECV)
-{
-  unsigned char c = UDR2;
-  store_char(c, &rx_buffer2);
-}
-
-SIGNAL(SIG_USART3_RECV)
-{
-  unsigned char c = UDR3;
-  store_char(c, &rx_buffer3);
-}
-
-#else
-
-#if defined(__AVR_ATmega8__)
-SIGNAL(SIG_UART_RECV)
-#else
-SIGNAL(USART_RX_vect)
-#endif
-{
-#if defined(__AVR_ATmega8__)
-  unsigned char c = UDR;
-#else
-  unsigned char c = UDR0;
-#endif
-  store_char(c, &rx_buffer);
-}
-
+#ifndef BAUD_TOL
+#   define BAUD_TOL 2
 #endif
 
-// Constructors ////////////////////////////////////////////////////////////////
+// both buffers are static global vars in this file. interrupt handlers
+// need to access them
+static rx_ring_buffer rx_buffer;
+static tx_ring_buffer tx_buffer;
 
-HardwareSerial::HardwareSerial(ring_buffer *rx_buffer,
-  volatile uint8_t *ubrrh, volatile uint8_t *ubrrl,
-  volatile uint8_t *ucsra, volatile uint8_t *ucsrb,
-  volatile uint8_t *udr,
-  uint8_t rxen, uint8_t txen, uint8_t rxcie, uint8_t udre, uint8_t u2x)
-{
-  _rx_buffer = rx_buffer;
-  _ubrrh = ubrrh;
-  _ubrrl = ubrrl;
-  _ucsra = ucsra;
-  _ucsrb = ucsrb;
-  _udr = udr;
-  _rxen = rxen;
-  _txen = txen;
-  _rxcie = rxcie;
-  _udre = udre;
-  _u2x = u2x;
+/**
+ * Initialize the USART module with the BAUD rate predefined
+ * in HardwareSerial.h
+ *
+ * This may implement passing addresses of registers, like the HardwareSerial
+ * class from the Arduino library, but that's not necessary at this point.
+ *
+ */
+HardwareSerial::HardwareSerial(rx_ring_buffer *rx_buffer_ptr, tx_ring_buffer *tx_buffer_ptr) {
+
+    _rx_buffer = rx_buffer_ptr;
+    _tx_buffer = tx_buffer_ptr;
+
+    // Allocate memory for the rx and tx buffers
+    // That is, if the structs contain pointers to the actual buffer
+    // arrays. I'm going for defining 2 separate struct's and allocating them
+    // statically.
+//    _rx_buffer->buffer = (uint8_t *)malloc(USART_RX_BUFFER_SIZE);
+//    _tx_buffer->buffer = (uint8_t *)malloc(USART_TX_BUFFER_SIZE);
 }
 
-// Public Methods //////////////////////////////////////////////////////////////
+void HardwareSerial::begin(unsigned long baud) {
+    // taken from <util/setbaud.h>
+    uint8_t use2x = 0;
+    uint16_t ubbr =  (F_CPU + 8UL * baud) / (16UL * baud) - 1UL;
+    if ( (100 * (F_CPU)) > (16 * (ubbr + 1) * (100 * ubbr + ubbr * BAUD_TOL)) ) {
+        use2x = 1;
+        ubbr = (F_CPU + 4UL * baud) / (8UL * baud) - 1UL;
+    }
 
-void HardwareSerial::begin(long baud)
-{
-  uint16_t baud_setting;
-  bool use_u2x;
+    UBRR0L = ubbr & 0xff;
+    UBRR0H = ubbr >> 8;
+    if (use2x) {
+        UCSR0A |= (1 << U2X0);
+    } else {
+        UCSR0A &= ~(1 << U2X0);
+    }
 
-  // U2X mode is needed for baud rates higher than (CPU Hz / 16)
-  if (baud > F_CPU / 16) {
-    use_u2x = true;
-  } else {
-    // figure out if U2X mode would allow for a better connection
-    
-    // calculate the percent difference between the baud-rate specified and
-    // the real baud rate for both U2X and non-U2X mode (0-255 error percent)
-    uint8_t nonu2x_baud_error = abs((int)(255-((F_CPU/(16*(((F_CPU/8/baud-1)/2)+1))*255)/baud)));
-    uint8_t u2x_baud_error = abs((int)(255-((F_CPU/(8*(((F_CPU/4/baud-1)/2)+1))*255)/baud)));
-    
-    // prefer non-U2X mode because it handles clock skew better
-    use_u2x = (nonu2x_baud_error > u2x_baud_error);
-  }
-  
-  if (use_u2x) {
-    *_ucsra = 1 << _u2x;
-    baud_setting = (F_CPU / 4 / baud - 1) / 2;
-  } else {
-    *_ucsra = 0;
-    baud_setting = (F_CPU / 8 / baud - 1) / 2;
-  }
+    // Flush buffers
+    _tx_buffer->head = _tx_buffer->tail = 0;
+    _rx_buffer->head = _rx_buffer->tail = 0;
 
-  // assign the baud_setting, a.k.a. ubbr (USART Baud Rate Register)
-  *_ubrrh = baud_setting >> 8;
-  *_ubrrl = baud_setting;
-
-  sbi(*_ucsrb, _rxen);
-  sbi(*_ucsrb, _txen);
-  sbi(*_ucsrb, _rxcie);
+    UCSR0B |= (1<<TXEN0);  // Enable Transmitter
+    UCSR0B |= (1<<RXEN0);  // Enable Reciever
+    UCSR0B |= (1<<RXCIE0); // Enable Rx Complete Interrupt
 }
 
-uint8_t HardwareSerial::available(void)
-{
-  return (RX_BUFFER_SIZE + _rx_buffer->head - _rx_buffer->tail) % RX_BUFFER_SIZE;
+void HardwareSerial::write(uint8_t data) {
+    // Calculate new head position
+    uint8_t tmp_head = (_tx_buffer->head + 1) & USART_TX_BUFFER_MASK;
+
+    // Block until there's room in the buffer
+    // XXX: this may block forever if someone externally disabled the transmitter
+    //      or the DRE interrupt and there's data in the buffer. Careful!
+    while (tmp_head == _tx_buffer->tail);
+
+    // Advance the head, store the data
+    _tx_buffer->buffer[tmp_head] = data;
+    _tx_buffer->head = tmp_head;
+
+    UCSR0B |= (1<<UDRIE0); // Enable Data Register Empty interrupt
 }
 
-int HardwareSerial::read(void)
-{
-  // if the head isn't ahead of the tail, we don't have any characters
-  if (_rx_buffer->head == _rx_buffer->tail) {
-    return -1;
-  } else {
-    unsigned char c = _rx_buffer->buffer[_rx_buffer->tail];
-    _rx_buffer->tail = (_rx_buffer->tail + 1) % RX_BUFFER_SIZE;
-    return c;
-  }
+void HardwareSerial::flush(void) {
+    // Not sure if disabling interrupts is needed here, but let's be on
+    // the safe side
+    // disable interrupts
+    uint8_t oldSREG = SREG;
+    cli();
+    _rx_buffer->head = _rx_buffer->tail;
+    // Re-enable interrupts
+    SREG = oldSREG;
 }
 
-void HardwareSerial::flush()
-{
-  // don't reverse this or there may be problems if the RX interrupt
-  // occurs after reading the value of rx_buffer_head but before writing
-  // the value to rx_buffer_tail; the previous value of rx_buffer_head
-  // may be written to rx_buffer_tail, making it appear as if the buffer
-  // don't reverse this or there may be problems if the RX interrupt
-  // occurs after reading the value of rx_buffer_head but before writing
-  // the value to rx_buffer_tail; the previous value of rx_buffer_head
-  // may be written to rx_buffer_tail, making it appear as if the buffer
-  // were full, not empty.
-  _rx_buffer->head = _rx_buffer->tail;
+/**
+ * Returns the number of bytes in the RX buffer
+ *
+ */
+uint8_t HardwareSerial::available(void) {
+    //    return (_rx_buffer->head + USART_RX_BUFFER_SIZE - _rx_buffer->tail) % USART_RX_BUFFER_SIZE;
+    // stupid compiler ...
+    uint16_t tmp = (_rx_buffer->head + USART_RX_BUFFER_SIZE - _rx_buffer->tail);
+    tmp %= USART_RX_BUFFER_SIZE;
+    return (uint8_t)tmp;
 }
 
-void HardwareSerial::write(uint8_t c)
-{
-  while (!((*_ucsra) & (1 << _udre)))
-    ;
+/**
+ * Returns a byte from the RX buffer, or NULL if none are available
+ */
+uint8_t HardwareSerial::read(void) {
+    uint8_t tmp_tail, tmp_data;
 
-  *_udr = c;
+    // disable interrupts
+    uint8_t oldSREG = SREG;
+    cli();
+    if (_rx_buffer->head == _rx_buffer->tail) {
+        // Better that than block the code waiting for data. Users should call
+        // available() first
+        tmp_data = -1;
+    } else {
+        tmp_tail = (_rx_buffer->tail +1) & USART_RX_BUFFER_MASK;
+        tmp_data = _rx_buffer->buffer[tmp_tail];
+        _rx_buffer->tail = tmp_tail;
+    }
+    // Re-enable interrupts
+    SREG = oldSREG;
+
+    return tmp_data;
 }
 
-// Preinstantiate Objects //////////////////////////////////////////////////////
+/**
+ * Receive handler
+ */
+ISR(USART_RX_vect) {
+    uint8_t data = UDR0;
+    uint8_t tmp_head = (rx_buffer.head + 1) & USART_RX_BUFFER_MASK;
+    if (tmp_head == rx_buffer.tail) {
+        // buffer overflow! for now, the strategy is to discard a byte off
+        // the queue, so that the fresh data can be written. Probably an overflow
+        // flag should be raised somewhere ...
+        // http://c2.com/cgi/wiki?CircularBuffer
+        rx_buffer.tail = (rx_buffer.tail + 1) & USART_RX_BUFFER_MASK;
+    }
+    rx_buffer.buffer[tmp_head] = data;
+    rx_buffer.head = tmp_head;
+}
 
-#if defined(__AVR_ATmega8__)
-HardwareSerial Serial(&rx_buffer, &UBRRH, &UBRRL, &UCSRA, &UCSRB, &UDR, RXEN, TXEN, RXCIE, UDRE, U2X);
-#else
-HardwareSerial Serial(&rx_buffer, &UBRR0H, &UBRR0L, &UCSR0A, &UCSR0B, &UDR0, RXEN0, TXEN0, RXCIE0, UDRE0, U2X0);
-#endif
+/**
+ * Data Register Empty Handler
+ */
+ISR(USART_UDRE_vect) {
+    if (tx_buffer.head == tx_buffer.tail) {
+        // Buffer is empty, disable the interrupt
+        UCSR0B &= ~(1<<UDRIE0);
+    } else {
+        tx_buffer.tail = (tx_buffer.tail + 1) & USART_TX_BUFFER_MASK;
+        UDR0 = tx_buffer.buffer[tx_buffer.tail];
+    }
+}
 
-#if defined(__AVR_ATmega1280__)
-HardwareSerial Serial1(&rx_buffer1, &UBRR1H, &UBRR1L, &UCSR1A, &UCSR1B, &UDR1, RXEN1, TXEN1, RXCIE1, UDRE1, U2X1);
-HardwareSerial Serial2(&rx_buffer2, &UBRR2H, &UBRR2L, &UCSR2A, &UCSR2B, &UDR2, RXEN2, TXEN2, RXCIE2, UDRE2, U2X2);
-HardwareSerial Serial3(&rx_buffer3, &UBRR3H, &UBRR3L, &UCSR3A, &UCSR3B, &UDR3, RXEN3, TXEN3, RXCIE3, UDRE3, U2X3);
-#endif
+HardwareSerial Serial(&rx_buffer, &tx_buffer);
--- HardwareSerial.h
+++ HardwareSerial.h
@@ -1,6 +1,6 @@
 /*
-  HardwareSerial.h - Hardware serial library for Wiring
-  Copyright (c) 2006 Nicholas Zambetti.  All right reserved.
+  HardwareSerial.h - Improved HardwareSerial library for Arduino
+  Copyright (c) 2009 Kiril Zyapkov.  All right reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -24,42 +24,52 @@
 
 #include "Print.h"
 
-struct ring_buffer;
+// Define the amount of tolerance upon which x2 will be enabled, in percent
+#define BAUD_TOL 2
+
+// Both buffers must be powers of 2, up to 128 bytes
+// Keep in mind the memory limits of the MCU!!
+#define USART_RX_BUFFER_SIZE   16
+#define USART_TX_BUFFER_SIZE   32
+
+// There were other ways of implementing the ring buffers that I thought of,
+// that avoid defining 2 separate struct's for the same thing:
+//
+//  * allocating memory for the buffers using malloc() at runtime
+//      not sure how 'dynamic' memory management will work on the micro
+//  * using separate variables, not packing the buffers into structs
+//      structs are much neater
+//  * defining a single macro for buffer size
+//      both buffers would need to have the same size
+struct rx_ring_buffer {
+    uint8_t buffer[USART_RX_BUFFER_SIZE];
+    volatile uint8_t head;
+    volatile uint8_t tail;
+};
+
+struct tx_ring_buffer {
+    uint8_t buffer[USART_TX_BUFFER_SIZE];
+    volatile uint8_t head;
+    volatile uint8_t tail;
+};
+
 
 class HardwareSerial : public Print
 {
   private:
-    ring_buffer *_rx_buffer;
-    volatile uint8_t *_ubrrh;
-    volatile uint8_t *_ubrrl;
-    volatile uint8_t *_ucsra;
-    volatile uint8_t *_ucsrb;
-    volatile uint8_t *_udr;
-    uint8_t _rxen;
-    uint8_t _txen;
-    uint8_t _rxcie;
-    uint8_t _udre;
-    uint8_t _u2x;
+    rx_ring_buffer *_rx_buffer;
+    tx_ring_buffer *_tx_buffer;
+
   public:
-    HardwareSerial(ring_buffer *rx_buffer,
-      volatile uint8_t *ubrrh, volatile uint8_t *ubrrl,
-      volatile uint8_t *ucsra, volatile uint8_t *ucsrb,
-      volatile uint8_t *udr,
-      uint8_t rxen, uint8_t txen, uint8_t rxcie, uint8_t udre, uint8_t u2x);
-    void begin(long);
-    uint8_t available(void);
-    int read(void);
+    HardwareSerial(rx_ring_buffer *, tx_ring_buffer *);
+    void begin(unsigned long);
+    void write(uint8_t);
     void flush(void);
-    virtual void write(uint8_t);
-    using Print::write; // pull in write(str) and write(buf, size) from Print
+    uint8_t available(void);
+    uint8_t read(void);
+
 };
 
 extern HardwareSerial Serial;
-
-#if defined(__AVR_ATmega1280__)
-extern HardwareSerial Serial1;
-extern HardwareSerial Serial2;
-extern HardwareSerial Serial3;
-#endif
 
 #endif
